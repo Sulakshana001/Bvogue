ddd51179bef7a5b546c40e530872adf3278a4b60
Sulakshana001 <140915442+Sulakshana001@users.noreply.github.com>
2025-08-06 16:11:58 +0530

    almost done

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { LoginpageComponent } from './app/loginpage/loginpage.component';
+import { MenComponent } from './app/men/men.component';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(LoginpageComponent, config);
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { LoginpageComponent } from './app/loginpage/loginpage.component';
+import { MenComponent } from './app/men/men.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication( LoginpageComponent, appConfig)
   .catch((err) => console.error(err));
